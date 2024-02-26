2038cd851e43ca033da0592b7298d40ec50ba821
Martin Cabalzar <m.cabalzar@gmx.ch>
2024-02-26 15:56:15 -0400

    Fixed unit tests after failed merge

--- tests/test_security.py
+++ tests/test_security.py
@@ -26,12 +26,11 @@ def test_encrypt_and_decrypt_email_address(monkeypatch, message):
     assert decrypted_message == message
 
 @pytest.mark.parametrize("ip_address, blocking_reason", [("155.255.152.55", BlockedIpReasonType.EXCESSIVE_FAILED_LOGIN_ATTEMPTS)])
-# @pytest.mark.parametrize("ip_address, blocking_reason", [("155.255.452.55", BlockedIpReasonType.EXCESSIVE_FAILED_LOGIN_ATTEMPTS)])
-# def test_block_ip_address(ip_address: str, blocking_reason: BlockedIpReasonType):
-#     ipSecurity = IPSecurity()
+def test_block_ip_address(ip_address: str, blocking_reason: BlockedIpReasonType):
+    ipSecurity = IPSecurity()
 
-#     assert ipSecurity.block_ip(ip_address, blocking_reason) is True
-#     assert ipSecurity.is_ip_blocked(ip_address) is True
+    assert ipSecurity.block_ip(ip_address, blocking_reason) is True
+    assert ipSecurity.is_ip_blocked(ip_address) is True
 
 
 @pytest.mark.parametrize("ip_address, blocking_reason", [("172.255.152.55", BlockedIpReasonType.BLACKLISTED_IP_ADDRESS)])
@@ -40,6 +39,8 @@ def test_block_and_unblock_ip_address(ip_address: str, blocking_reason: BlockedI
 
     assert ipSecurity.block_ip(ip_address, blocking_reason) is True
     assert ipSecurity.is_ip_blocked(ip_address) is True
+    assert ipSecurity.unblock_ip(ip_address) is True
+    assert ipSecurity.is_ip_blocked(ip_address) is False
 
 @pytest.mark.parametrize("ip_address", [("11.255.152.55")])
 def test_unblock_non_blocked_ip_address(ip_address: str):
