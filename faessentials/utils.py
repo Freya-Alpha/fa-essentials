469742a7d31013a15606bd3091e34953d1a38765
svabra <brayan@brayan.com>
2024-06-22 17:33:08 +0200

    formatting

--- faessentials/utils.py
+++ faessentials/utils.py
@@ -5,7 +5,6 @@ import yaml
 
 PROJECT_ROOT = None
 
-
 # Determine the project root path when the module is loaded
 def find_project_root(current_path: pathlib.Path, max_depth: int = 10) -> pathlib.Path:
     """
@@ -26,15 +25,12 @@ def find_project_root(current_path: pathlib.Path, max_depth: int = 10) -> pathli
                             Ensure the 'config' or 'logs' folder exists in {str(current_path)}. \
                             The PROJECT_ROOT environment variable is: {project_root_env}")
 
-
 # Initialize PROJECT_ROOT when the module is loaded
 def initialize_project_root():
     global PROJECT_ROOT
     PROJECT_ROOT = find_project_root(pathlib.Path(os.getcwd()).resolve())
 
-
 initialize_project_root()
-
 
 def get_project_root_path() -> Path:
     """
@@ -42,22 +38,18 @@ def get_project_root_path() -> Path:
     """
     return PROJECT_ROOT
 
-
 def get_project_root() -> str:
     str_path = str(PROJECT_ROOT)
     # print(f"utils.py: {str_path}")
     return str_path
 
-
 def get_log_path() -> Path:
     abs_path = get_project_root_path().joinpath("logs")
     return abs_path
 
-
 def get_secrets_path() -> Path:
     abs_path = get_project_root_path().joinpath("secrets")
     return abs_path
-
 
 def get_app_config() -> dict:
     app_cfg = None
@@ -72,13 +64,11 @@ def get_app_config() -> dict:
         )
     return app_cfg
 
-
 def get_application_name() -> str:
     app_name = get_app_config().get("application")
     if app_name is None:
         raise ValueError("Application name not found in app_config.")
     return app_name
-
 
 def get_domain_name() -> str:
     domain_name = get_app_config().get("domain")
@@ -86,21 +76,17 @@ def get_domain_name() -> str:
         raise ValueError("Domain name not found in app_config.")
     return domain_name
 
-
 def get_environment() -> str:
     """Will fetch the environment variable ENV. If not present it will fall back to DEV """
     return os.environ.get("ENV", "DEV")
-
 
 def get_service_url() -> str:
     """This own service url value. This global environment variable is usually used by consumers apps of this API."""
     return os.getenv("OPENAPI_SERVICE_URL", "http://localhost:8080")
 
-
 def get_service_doc_url() -> str:
     """Return the OpenAPI url"""
     return f"{get_service_url}/docs"
-
 
 def get_logging_level() -> str:
     return get_app_config().get("logging_level", os.getenv("LOGGING_LEVEL", "DEBUG")).upper()
